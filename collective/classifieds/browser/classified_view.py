7a6de47239b10c269e8c31c6b7b702719e298c94
Cesar Augusto <cesaraugusto@liberiun.com>
2014-01-17 14:37:34 -0200

    Correção dos separadores de casas decimais no classificado

--- collective/classifieds/browser/classified_view.py
+++ collective/classifieds/browser/classified_view.py
@@ -5,6 +5,19 @@ from Products.Five import BrowserView
 
 
 class classified_view(BrowserView):
-    """
-        BrowserView for classified
-    """
+	"""
+		BrowserView for classified
+	"""
+
+	def format_moeda(self, valor):
+		import locale
+		locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+		return  locale.currency( float(valor), grouping=True)
+
+
+	def get_price(self):
+		valor = self.context.price
+		if valor:
+			return self.format_moeda(valor)
+		else:
+			return 'R$ 0,00'
